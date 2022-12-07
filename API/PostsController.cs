e941110718042609a38ea04024da96f4a0368bd3
unknown <velazql7@gmatc.matc.edu>
2022-12-06 22:02:08 -0600

    Completed Activity 6

--- API/PostsController.cs
+++ API/PostsController.cs
@@ -25,5 +25,38 @@ namespace API.Controllers
         {
             return this.context.Posts.ToList();
         }
+
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this.context.Posts.Find(id);
+            if (post is null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+        [HttpPost(Name = "Create")]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                //Date = request.Date
+
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(post);
+            }
+            throw new Exception("Error creating post");
+
+        }
     }
 }
